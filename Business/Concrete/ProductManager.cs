29ab590d63e75de0218f8eebc43178a2ee9edc48
serhatsevim <serhatsevim01@gmail.com>
2021-02-26 22:52:14 +0200

    Kurumsal Katmanlı Mimari 1. Ders

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,24 @@
+using Business.Abstract;
+using Entites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete{
+	public class ProductManager : IProductService
+	{	
+		IProductDal _productDal;
+		
+		public ProductManager(IProductDal productDal)
+		{
+			_productDal = productDal;
+		}
+		public List<Product> GetAll()
+		{
+			//İş kodları
+			//Yetkisi var mı?
+			
+			return _productDal.GetAll();
+		}
+	}
+}
--- Entities/Abstract/IEntity.cs
+++ Entities/Abstract/IEntity.cs
@@ -1,1 +1,11 @@
-1
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+	//IEntity implement eden class bir veritabanı tablosudur
+	public interface IEntity
+	{		
+	}
+}
